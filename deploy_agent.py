cebab52bd22c4420a0447cdcac46927a876854b0
Adrian Albu <2412rock@gmail.com>
2024-05-02 13:04:46 +0300

    fixes

--- deploy_agent.py
+++ deploy_agent.py
@@ -17,25 +17,17 @@ def post_data():
     password = f.readline()
     f.close()
     if password == data["password"]:
-        deploy_fe = data["deploy_frontend"]
         deploy_be = data["deploy_backend"]
-        should_deploy_redis = data["deploy_redis"]
         should_deploy_sql = data["deploy_sql"]
-        should_deploy_minio = data["deploy_minio"]
-
-        deploy_sql_migrations()
+        #should_deploy_minio = data["deploy_minio"]
         
-        if should_deploy_redis:
-            deploy_redis_server()
-        if should_deploy_sql:
-            deploy_sql_server()
-        if deploy_fe:
-            deploy_frontend()
         if deploy_be:
             deploy_backend()
-        if should_deploy_minio:
-            deploy_minio_server()
-
+        if should_deploy_sql:
+            deploy_sql_server()
+        # if should_deploy_minio:
+        #     deploy_minio_server()
+        deploy_sql_migrations()
         # Return a JSON response
         return jsonify("ok"), 200
     return jsonify("Bad password"), 400
@@ -53,10 +45,10 @@ def deploy_sql_migrations():
     os.chdir("/home/app/code/sql-overflow")
 
     os.system("docker cp init.sql sql-server:/usr/src")
-    os.system("docker cp pupulate_with_data.sql sql-server:/usr/src")
+    os.system("docker cp populate.sql sql-server:/usr/src")
     #docker exec -it sql-server /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P MyP@ssword1! -d master -i /usr/src/init.sql
     os.system(f"docker exec -it sql-server /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P {getSqlPassword()} -d master -i /usr/src/init.sql")
-    os.system(f"docker exec -it sql-server /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P {getSqlPassword()} -d master -i /usr/src/pupulate_with_data.sql")
+    os.system(f"docker exec -it sql-server /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P {getSqlPassword()} -d master -i /usr/src/populate.sql")
 
 
 def deploy_sql_server():
