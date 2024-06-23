06ee49d27be6c60c9309dbddf36059479fc8d160
Adrian Albu <2412rock@gmail.com>
2024-06-23 23:08:21 +0300

    deploy windows

--- deploy_agent_windows.py
+++ deploy_agent_windows.py
@@ -13,18 +13,21 @@ app = Flask(__name__)
 def post_data():
     # Get the JSON data from the request
     data = request.get_json()
-    f = open('C:/Users/Server/Documents/password.txt', 'r')
+    f = open('C:/Users/Server/Documents/overflow/password.txt', 'r')
     password = f.readline()
     f.close()
     if password == data["password"]:
         deploy_be = data["deploy_backend"]
         should_deploy_sql = data["deploy_sql"]
+        should_deploy_bot = data["deploy_bot"]
         #should_deploy_minio = data["deploy_minio"]
         
         if deploy_be:
             deploy_backend()
         if should_deploy_sql:
             deploy_sql_server()
+        if should_deploy_bot:
+            deploy_bot()
         # if should_deploy_minio:
         #     deploy_minio_server()
         deploy_sql_migrations()
@@ -33,10 +36,29 @@ def post_data():
     return jsonify("Bad password"), 400
 
 def getSqlPassword():
-    f = open('C:/Users/Server/Documents/sql_password.txt', 'r')
+    f = open('C:/Users/Server/Documents/overflow/sql_password.txt', 'r')
     password = f.readline()
     f.close()
     return password
+
+def readLineFromFile(file):
+    f = open(file)
+    result = f.readline()
+    f.close()
+    return result
+
+
+def deploy_bot():
+    os.chdir("C:/Users/Server/Desktop")
+    os.system("rm -rf overflow-bot")
+    os.system("git clone https://github.com/2412rock/overflow-bot")
+    os.chdir("C:/Users/Server/Desktop/overflow-bot")
+    os.system("docker stop bot")
+    os.system("docker rm bot")
+    os.system("docker build -t bot .")
+    password = readLineFromFile("C:/Users/Server/Documents/overflow/bot_password.txt")
+    subprocess.Popen([
+    "docker", "run", "-e", f"PASSWORD={password}", "--name", "bot", "bot"])
 
 def deploy_sql_migrations():
     os.chdir("C:/Users/Server/Desktop")
@@ -72,28 +94,22 @@ def deploy_sql_server():
     os.system(f"docker exec -it sql-server /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P {getSqlPassword()} -d master -i /usr/src/init.sql")
     os.system(f"docker exec -it sql-server /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P {getSqlPassword()} -d master -i /usr/src/pupulate_with_data.sql")
 
-def readLineFromFile(file):
-    f = open(file)
-    result = f.readline()
-    f.close()
-    return result
-
 def deploy_backend():
     os.chdir("C:/Users/Server/Desktop")
     os.system("rm -rf overflow-backend")
     os.system("git clone https://github.com/2412rock/overflow-backend")
-    shutil.copy("C:/Users/Server/Desktop/backendcertificate.pfx", "C:/Users/Server/Desktop/overflow-backend")
+    shutil.copy("C:/Users/Server/Documents/overflow/backendcertificate.pfx", "C:/Users/Server/Desktop/overflow-backend")
     os.chdir("C:/Users/Server/Desktop/overflow-backend")
     os.system("docker stop backend")
     os.system("docker rm backend")
     os.system("docker build -t backend .")
 
-    pfx_pass_file = open('C:/Users/Server/Documents/pfx_pass.txt', 'r')
+    pfx_pass_file = open('C:/Users/Server/Documents/overflow/pfx_pass.txt', 'r')
     pfx_pass = pfx_pass_file.readline()
     pfx_pass_file.close()
 
-    jwt_secret = readLineFromFile("C:/Users/Server/Documents/JWT_SECRET.txt")
-    minio_password = readLineFromFile("C:/Users/Server/Documents/minio_password.txt")
+    jwt_secret = readLineFromFile("C:/Users/Server/Documents/overflow/JWT_SECRET.txt")
+    minio_password = readLineFromFile("C:/Users/Server/Documents/overflow/minio_password.txt")
     subprocess.Popen(["docker", "run", "-e", f'SA_PASSWORD={getSqlPassword()}',
                        "-e", f'PFX_PASS={pfx_pass}',
                        "-e", f"JWT_SECRET={jwt_secret}",
@@ -101,4 +117,4 @@ def deploy_backend():
                          "--name" ,"backend", "-p" ,"4200:4200" ,"backend"])
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="80",debug=True)
+    app.run(host="172.26.17.97", port="80",debug=True)
